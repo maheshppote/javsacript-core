9196d8686fd4660cd8e2f9ae5e51cd608dc34258
Mahesh pote <mpote97@gmail.com>
2025-01-28 19:41:15 +0530

    Added controls flow folder

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,29 @@
+let month = 7;
+
+switch (month) {
+  case 1:
+    console.log("January");
+    break;
+  case 2:
+    console.log("feb");
+    break;
+  case 3:
+    console.log("March");
+    break;
+  case 4:
+    console.log("april");
+    break;
+  case 5:
+    console.log("May");
+    break;
+  case 6:
+    console.log("Jun");
+    break;
+  case 7:
+    console.log("July");
+    break;
+
+  default:
+    console.log("No month required")
+    break;
+}
